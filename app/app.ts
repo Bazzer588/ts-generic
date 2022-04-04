07f3937777a591969c24d24363edfcd12bf7f208
Bazzer588 <40038437+Bazzer588@users.noreply.github.com>
2022-04-04 22:55:47 +0100

    Lint

--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,21 @@
-import express, {Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
+
+const PORT = 3033;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-    return res.send({text: 'Hello', date: new Date()});
+  return res.send({ text: 'Hello', date: new Date() });
+});
+
+// error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => { // eslint-disable-line  @typescript-eslint/no-unused-vars
+  console.error(err.stack);
+  res.status(500).send('Server Error');
 });
 
 app.listen(3033, () => {
-    console.log('Hello ' + 3033 + ' ' + new Date());
+  console.log('TS Server ' + PORT + ' ' + new Date());
 });
